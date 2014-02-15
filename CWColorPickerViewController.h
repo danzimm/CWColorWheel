939c1c0fb6db163ca2a354d5fbf1d8d49b4cdcab
Dan Zimmerman <daniel.zimmerman@me.com>
2014-02-14 20:56:20 -0600

    Updated comments

--- CWColorPickerViewController.h
+++ CWColorPickerViewController.h
@@ -6,6 +6,14 @@
 //  Copyright (c) 2013 Dan Zimmerman. All rights reserved.
 //
 
+/*
+ * @zdocs filedescription
+ *
+ * @title CWColorPickerViewController Class Reference
+ *
+ * @@ Use this class to display the color wheel. Future versions may support multiple types of color pickers.
+ */
+
 #import <UIKit/UIKit.h>
 
 @protocol CWColorPickerViewControllerDelegate;
@@ -18,15 +26,34 @@
     
     UIView *colorSwatch;
 }
-
+/*
+ * @description The delegate for this color picker.
+ *
+ * @discussion Set the delegate in order to get events about when a color is picked and when it is changed.
+ */
 @property (nonatomic) id <CWColorPickerViewControllerDelegate> delegate;
+/*
+ * @description Set or get the current color for the color picker.
+ *
+ * @discussion Setting this updates the color picker's UI
+ */
 @property (nonatomic) UIColor *color;
 
 @end
 
 @protocol CWColorPickerViewControllerDelegate <NSObject>
 @required
+/*
+ * @description The callback when the user taps the 'done' button.
+ *
+ * @discussion One needs to manually dismiss this viewcontroller, usually after this method is called.
+ */
 - (void)colorPicker:(CWColorPickerViewController *)cp pickedColor:(UIColor *)color;
+/*
+ * @description The callback when the user changes the color (when he taps on the color wheel or moves his finger).
+ *
+ * @discussion This gets called often, so don't do any heavy lifting here!
+ */
 - (void)colorPicker:(CWColorPickerViewController *)cp colorChanged:(UIColor *)color;
 
 @end
--- CWColorWheelView.h
+++ CWColorWheelView.h
@@ -6,6 +6,14 @@
 //  Copyright (c) 2013 Dan Zimmerman. All rights reserved.
 //
 
+/*
+ * @zdocs filedescription
+ *
+ * @title CWColorWheelView Class Reference
+ *
+ * @@ The view works on an HSB coordinate system, so you need to set the brightness and alpha for full control out of the color you get. Note that the API for this view is unstable thus it is not recommended that you use this class directly. It is suggested that you use the view controller instead.
+ */
+
 #import <UIKit/UIKit.h>
 
 @class DZMagnifyingView;
@@ -16,12 +24,27 @@
     DZMagnifyingView *magnifier;
 }
 
+/*
+ * @description Set this to change the color wheel's brightness
+ */
 @property (nonatomic) CGFloat HSBBrightness;
+/*
+ * @description Set this to change the color wheel's alpha.
+ */
 @property (nonatomic) CGFloat HSBAlpha;
 
+/*
+ * @description The block called when the user taps or moves to a different color.
+ */
 @property (nonatomic, copy) void (^colorPickedHandler)(UIColor *color);
 
+/*
+ * @description Call this once you are done changing `HSBBrightness` and `HSBAlpha`, otherwise the magnifier will be magnifying an old color wheel with different values of `HSBBrightness` and `HSBAlpha`.
+ */
 - (void)updateCachedWheel;
+/*
+ * @description Call this when the color wheel is being taken off screen. It forces the magnifier to hide, otherwise it may stay open.
+ */
 - (void)close;
 
 @end
